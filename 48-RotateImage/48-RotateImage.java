5af20e53f2bd3565a61e2b3025dbb740b125c8d4
ABINAYA M <beni051023@gmail.com>
2026-08-11 22:08:24 +0530

    Create 48-RotateImage

---
+++ 48-RotateImage/48-RotateImage.java
@@ -0,0 +1,22 @@
+// Last updated: 8/11/2026, 10:08:22 PM
+class Solution {
+    public void rotate(int[][] a) {
+        int n = a.length;
+        for (int i = 0; i < n; i++)
+            for (int j = i; j < n; j++) {
+                int t = a[i][j];
+                a[i][j] = a[j][i];
+                a[j][i] = t;
+            }
+        for (int i = 0; i < n; i++) {
+            int l = 0, r = n - 1;
+            while (l < r) {
+                int t = a[i][l];
+                a[i][l] = a[i][r];
+                a[i][r] = t;
+                l++;
+                r--;
+            }
+        }
+    }
+}
