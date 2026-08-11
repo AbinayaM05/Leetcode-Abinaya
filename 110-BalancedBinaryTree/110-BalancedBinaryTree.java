b92ecacc7685b0ca64a46c8092b7f272561020a0
ABINAYA M <beni051023@gmail.com>
2026-08-11 22:06:53 +0530

    Create 110-BalancedBinaryTree

---
+++ 110-BalancedBinaryTree/110-BalancedBinaryTree.java
@@ -0,0 +1,22 @@
+// Last updated: 8/11/2026, 10:06:53 PM
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return check(root) != -1;
+    }
+
+    private int check(TreeNode node) {
+        if (node == null) return 0;
+
+        int left_h = check(node.left);
+        if (left_h == - 1) return -1;
+
+        int right_h = check(node.right);
+        if (right_h == -1) return -1;
+
+        if (Math.abs(left_h - right_h) > 1) {
+            return -1;
+        }
+
+        return Math.max(left_h, right_h) + 1;
+    }
+}
