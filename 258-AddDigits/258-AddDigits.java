54fefc13b2430bcf8c9824e94d64546a958d35a3
ABINAYA M <beni051023@gmail.com>
2026-08-11 22:03:55 +0530

    Create 258-AddDigits

---
+++ 258-AddDigits/258-AddDigits.java
@@ -0,0 +1,14 @@
+// Last updated: 8/11/2026, 10:03:54 PM
+class Solution {
+    public int addDigits(int num) {
+        while (num >= 10) {
+            int sum = 0;
+            while (num > 0) {
+                sum = sum + (num % 10);
+                num = num / 10;
+            }
+            num = sum;
+        }
+        return num;
+    }
+}
