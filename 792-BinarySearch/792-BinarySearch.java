fbeccd9a707c8cc967e703c2f4dd2dbb901246a5
ABINAYA M <beni051023@gmail.com>
2026-08-11 21:57:08 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.java
@@ -0,0 +1,19 @@
+// Last updated: 8/11/2026, 9:57:08 PM
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+
+        while (l <= r) {
+            int m = (l + r) / 2;
+
+            if (nums[m] == target)
+                return m;
+            else if (nums[m] < target)
+                l = m + 1;
+            else
+                r = m - 1;
+        }
+
+        return -1;
+    }
+}
