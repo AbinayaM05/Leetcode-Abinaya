0f26a14449845382d428ffe4f023b6d3274bd31a
ABINAYA M <beni051023@gmail.com>
2026-08-11 22:02:44 +0530

    Create 394-DecodeString

---
+++ 394-DecodeString/394-DecodeString.java
@@ -0,0 +1,28 @@
+// Last updated: 8/11/2026, 10:02:44 PM
+class Solution {
+    public String decodeString(String str) {
+       Stack<Integer> numSt=new Stack<>();
+       Stack<String> st=new Stack<>();
+       int n=0;
+       String s="";
+       for(char c:str.toCharArray()){
+        if(Character.isDigit(c))
+        n=n*10+c-'0';
+        else if(Character.isAlphabetic(c))
+        s+=c;
+        else if(c=='['){
+            numSt.push(n);
+            st.push(s);
+            n=0;
+            s="";
+        }else{
+            StringBuilder temp=new StringBuilder();
+            int t=numSt.pop();
+            for(int i=0;i<t;i++)
+            temp.append(s);
+            s=st.pop()+temp.toString();
+            }
+        }
+        return s;
+    }  
+}
