929cdecb3deb52a8ebe6a9511f81296f82c91caf
ABINAYA M <beni051023@gmail.com>
2026-08-11 21:51:01 +0530

    Create 4363-MinimumTotalPriceAfterApplyingDiscounts

---
+++ 4363-MinimumTotalPriceAfterApplyingDiscounts/4363-MinimumTotalPriceAfterApplyingDiscounts.java
@@ -0,0 +1,21 @@
+// Last updated: 8/11/2026, 9:51:01 PM
+import java.util.Arrays;
+class Solution {
+    public double minPrice(int[] prices, int[] discounts) {
+        Arrays.sort(prices);
+        Arrays.sort(discounts);
+        double total=0;
+        for(int price:prices){
+            total+=price;
+        }
+        int i=prices.length-1;
+        int j=discounts.length-1;
+        while(i>=0&&j>=0){
+            total-=(double)prices[i]*discounts[j]/100;
+            i--;
+            j--;
+        }
+            return total;
+         
+    }
+}
