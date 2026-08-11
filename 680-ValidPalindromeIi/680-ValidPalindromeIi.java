c8c00611623fe38a267562da86aeb46c5bef414c
ABINAYA M <beni051023@gmail.com>
2026-08-11 21:58:38 +0530

    Create 680-ValidPalindromeIi

---
+++ 680-ValidPalindromeIi/680-ValidPalindromeIi.java
@@ -0,0 +1,26 @@
+// Last updated: 8/11/2026, 9:58:38 PM
+class Solution {
+    public boolean validPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+              
+                return isPal(s, left + 1, right) || isPal(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+        return true; 
+    }
+
+    private boolean isPal(String s, int i, int j) {
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
