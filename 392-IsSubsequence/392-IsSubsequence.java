db5dc4b4c458b0f65c5147f1709ac412b225daa3
ABINAYA M <beni051023@gmail.com>
2026-08-11 22:02:47 +0530

    Create 392-IsSubsequence

---
+++ 392-IsSubsequence/392-IsSubsequence.java
@@ -0,0 +1,14 @@
+// Last updated: 8/11/2026, 10:02:47 PM
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int i = 0, j = 0;
+
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j))
+                i++;
+            j++;
+        }
+
+        return i == s.length();
+    }
+}
