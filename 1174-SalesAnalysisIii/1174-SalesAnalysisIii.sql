8186a6a5cd951dbd5ec3c935f80eb18de900e7fa
ABINAYA M <beni051023@gmail.com>
2026-08-11 21:55:28 +0530

    Create 1174-SalesAnalysisIii

---
+++ 1174-SalesAnalysisIii/1174-SalesAnalysisIii.sql
@@ -0,0 +1,9 @@
+-- Last updated: 8/11/2026, 9:55:28 PM
+SELECT p.product_id,
+       p.product_name
+FROM Product p
+JOIN Sales s
+ON p.product_id = s.product_id
+GROUP BY p.product_id, p.product_name
+HAVING MIN(s.sale_date) >= '2019-01-01'
+   AND MAX(s.sale_date) <= '2019-03-31';
