dccd2ac12ba9c7423d613e63a0afc5d2cf7d13a4
ABINAYA M <beni051023@gmail.com>
2026-08-11 21:51:44 +0530

    Create 2903-InsertGreatestCommonDivisorsInLinkedList

---
+++ 2903-InsertGreatestCommonDivisorsInLinkedList/2903-InsertGreatestCommonDivisorsInLinkedList.java
@@ -0,0 +1,21 @@
+// Last updated: 8/11/2026, 9:51:43 PM
+class Solution {
+    public ListNode insertGreatestCommonDivisors(ListNode head) {
+        ListNode cur = head;
+
+        while (cur.next != null) {
+            int a = cur.val,
+                b = cur.next.val;
+
+            while (b != 0) {
+                int t = b;
+                b = a % b;
+                a = t;
+            }
+
+            cur.next = new ListNode(a, cur.next);
+            cur = cur.next.next;
+        }
+        return head;
+    }
+}
