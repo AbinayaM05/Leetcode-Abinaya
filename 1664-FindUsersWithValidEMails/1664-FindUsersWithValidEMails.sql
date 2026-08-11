25fdf94a6ac27f3444a7a0549e5880ed24ff7c3f
ABINAYA M <beni051023@gmail.com>
2026-08-11 21:53:57 +0530

    Create 1664-FindUsersWithValidEMails

---
+++ 1664-FindUsersWithValidEMails/1664-FindUsersWithValidEMails.sql
@@ -0,0 +1,10 @@
+-- Last updated: 8/11/2026, 9:53:57 PM
+SELECT 
+    user_id, 
+    name, 
+    mail
+FROM 
+    users
+WHERE 
+    mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode\\.com$' 
+    AND mail LIKE BINARY '%@leetcode.com';
