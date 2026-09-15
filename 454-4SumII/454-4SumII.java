4b6dafb6b692b6f69fef78a98cf84f6965cbd6e3
ABINAYA M <beni051023@gmail.com>
2026-09-15 14:39:12 +0530

    Create 454-4SumII

---
+++ 454-4SumII/454-4SumII.java
@@ -0,0 +1,19 @@
+// Last updated: 9/15/2026, 2:39:11 PM
+1class Solution {
+2    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+3        int res = 0;
+4        Map<Integer, Integer> map = new HashMap<>();
+5        for(int a: nums1) {
+6            for(int b: nums2) {
+7                map.put(a+b, map.getOrDefault(a+b, 0)+1);
+8            }
+9        }
+10
+11        for(int c: nums3) {
+12            for(int d: nums4) {
+13                res += map.getOrDefault(-(c+d), 0);
+14            }
+15        }
+16        return res;
+17    }
+18}
